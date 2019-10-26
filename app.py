a30babb6c11ecdee31acbde26c726d1c97a63cd8
unknown <andrew.p.spatz@gmail.com>
2019-10-26 12:34:15 -0400

    finishing app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
@@ -85,15 +86,75 @@ def stations():
 
 
 @app.route("/api/v1.0/tobs")
+def temps():
+    session = Session(engine)
+    """Getting year info from data"""
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date = np.ravel(last_date)
+    date = last_date[0].split('-')
+    year = int(date[0])
+    month = int(date[1])
+    day = int(date[2])
+    format_date = dt.date(year, month, day)
+    tdelta = dt.timedelta(days=-365)
+    last_year = format_date + tdelta
+
+    """Getting Temp data for year"""
+    results = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date.between(last_year, format_date)).group_by(Measurement.date).all()
+    session.close()
+
+    """Preparing data"""
+    temp_data = []
+    for date, temp in results:
+        day_dict={}
+        day_dict['date']=date
+        day_dict['temp']=temp
+        temp_data.append(day_dict)
+    
+    return jsonify(temp_data)
 
 
-"""
 @app.route("/api/v1.0/<start>")
+def startdate(start):
+    """Getting temp data starting from start date"""
+    session = Session(engine)
+    tmin = session.query(Measurement.tobs).filter(Measurement.date>=start).order_by(Measurement.tobs.asc()).first()
+    tmax = session.query(Measurement.tobs).filter(Measurement.date>=start).order_by(Measurement.tobs.desc()).first()
+    tavg = session.query(func.avg(Measurement.tobs)).filter(Measurement.date>=start).order_by(Measurement.tobs.desc()).all()
+    session.close()
+
+    """Formatting data"""
+    temp_data = {}
+    temp_data['start date']=start
+    temp_data['temps']={}
+    temp_data['temps']['min']=tmin
+    temp_data['temps']['max']=tmax
+    temp_data['temps']['avg']=tavg
+
+    return jsonify(temp_data)
 
 
-""""""
 @app.route("/api/v1.0/<start>/<end>")
-"""
+def startenddate(start,end):
+    """Getting temp data starting from start date"""
+    session = Session(engine)
+    tmin = session.query(Measurement.tobs).filter(Measurement.date.between(start,end)).order_by(Measurement.tobs.asc()).first()
+    tmax = session.query(Measurement.tobs).filter(Measurement.date.between(start,end)).order_by(Measurement.tobs.desc()).first()
+    tavg = session.query(func.avg(Measurement.tobs)).filter(Measurement.date.between(start,end)).order_by(Measurement.tobs.desc()).all()
+    session.close()
+
+    """Formatting data"""
+    temp_data = {}
+    temp_data['dates']={}
+    temp_data['dates']['start']=start
+    temp_data['dates']['end']=end
+    temp_data['temps']={}
+    temp_data['temps']['min']=tmin
+    temp_data['temps']['max']=tmax
+    temp_data['temps']['avg']=tavg
+
+    return jsonify(temp_data)
 
 
 
